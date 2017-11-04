6551a4d24d2179e4fd0a202b401469882b1003f4
SanAndreasP <1294@live.de>
2017-11-04 18:33:19 +0100

    changed MC version number

--- src/main/java/de/sanandrew/mods/sanlib/SanLib.java
+++ src/main/java/de/sanandrew/mods/sanlib/SanLib.java
@@ -25,7 +25,7 @@ public class SanLib
     public static final String ID = "sanlib";
     public static final String VERSION = "1.3.0";
     public static final String CHANNEL = "SanLibNWCH";
-    public static final String MCVER = "[1.11, 1.12)";
+    public static final String MCVER = "[1.11.2, 1.12)";
     public static final String DEPENDENCIES = "required-after:forge@[13.19.1.2199,]";
 
     public static final Logger LOG = LogManager.getLogger(ID);
