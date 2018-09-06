e70aa4e76289ca372c4794bc4fd34a2aec1f64ce
SanAndreasP <1294@live.de>
2018-09-06 19:44:32 +0200

    jars are now signed \o/

--- src/main/java/de/sanandrew/mods/sanlib/SanLib.java
+++ src/main/java/de/sanandrew/mods/sanlib/SanLib.java
@@ -8,18 +8,21 @@ package de.sanandrew.mods.sanlib;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = SanLib.ID, version = SanLib.VERSION, name = SanLib.NAME, acceptedMinecraftVersions = SanLib.MCVER, dependencies = SanLib.DEPENDENCIES,
-     acceptableRemoteVersions = SanLib.ACCEPTED_REMOTE_VER)
+     acceptableRemoteVersions = SanLib.ACCEPTED_REMOTE_VER, certificateFingerprint = SanLib.CERTIFICATE_FINGERPRINT)
 public class SanLib
 {
     public static final String ID = "sanlib";
     public static final String NAME = "San's Library";
+    public static final String CERTIFICATE_FINGERPRINT = "df48348748b5e141b1d118e2302a8d5be930b708";
     static final String VERSION = "1.5.1";
     static final String ACCEPTED_REMOTE_VER = "[1.5.1,)";
     static final String MCVER = "[1.12.2, 1.13)";
@@ -52,5 +55,10 @@ public class SanLib
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event) {
         proxy.postInit(event);
+    }
+
+    @Mod.EventHandler
+    public void onFingerprintViolation(FMLFingerprintViolationEvent event) {
+        LOG.log(Level.ERROR, "Invalid Fingerprint detected! The file " + event.getSource().getName() + " may have been tampered with. This version will NOT be supported by the author");
     }
 }
